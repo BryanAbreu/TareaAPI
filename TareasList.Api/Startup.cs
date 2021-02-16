5edd3291a039fc294fcffd0c8eb1ff573009c02c
Bryan Abreu <batabreu13@gmail.com>
2021-02-15 21:30:13 -0400

    solution

--- TareasList.Api/Startup.cs
+++ TareasList.Api/Startup.cs
@@ -46,7 +46,7 @@ namespace TareasList.Api
             .AddFluentValidation(options =>
             {
               options.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
-            });
+            });   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
